0929c46ffe3df465d1e3feb42781aecf3350438a
umesh kadam <umeshk149@gmail.com>
2021-10-04 09:45:53 +0530

    removed ./docs

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo "Deploying....."
+echo "https://umeshmk.github.io/technotes/" 
+# echo "https://umeshmk.github.io/<REPO>/" > CNAME
+
+git init
+git add -A
+git commit -m "deploy" &&
+
+git push -f git@github.com:umeshmk/rp-vite-redux-counter.git master:gh-pages
+
+cd -
